57e2fa90d6e5ad4fd57bb4341af15bc1c6a0499d
Nicolas Avila <nicolasavila.a@gmail.com>
2019-01-26 00:10:27 -0300

    feat: commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Agrega estos módulos
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
+//Formularios
+import { ReactiveFormsModule } from '@angular/forms';
+//Servicio
+import { FirebaseStorageService } from './firebase-storage.service';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +17,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireStorageModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    FirebaseStorageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
